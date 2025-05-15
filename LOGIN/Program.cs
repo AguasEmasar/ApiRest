2c05f392b446e85b6f56c70467eafdacbeece3f6
AguasEmasar <aguassantarosa6@gmail.com>
2025-05-15 10:32:38 -0600

    Update Program.cs

--- LOGIN/Program.cs
+++ LOGIN/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Identity;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
@@ -17,38 +16,32 @@ builder.Configuration
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .CreateLogger();
+builder.Host.UseSerilog(); // Integración Serilog con la aplicación
 
-builder.Host.UseSerilog(); // Ingracion Serilog con la aplicación
+// Configurar URL una sola vez
 builder.WebHost.UseUrls("http://0.0.0.0:4000");
 
 try
 {
     var startup = new Startup(builder.Configuration);
     startup.ConfigureServices(builder.Services);
-
-    builder.WebHost.UseUrls("http://*:4000");
-
+    
     var app = builder.Build();
+    
+    // Configurar redirección de la raíz a Swagger
+    app.MapGet("/", () => Results.Redirect("/swagger/index.html"));
+    
     startup.Configure(app, app.Environment);
-
     await InitializeDatabaseAsync(app);
-
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseHttpsRedirection();
-    app.UseAuthentication();
-    app.UseAuthorization();
-    app.MapControllers();
-
+    
     DotEnv.Load(options: new DotEnvOptions(probeForEnv: true));
-
+    
     // Middleware para logging de requests
     app.UseSerilogRequestLogging(options =>
     {
         options.MessageTemplate = "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
     });
-    app.Urls.Add("http://*:4000");
-
+    
     app.Run();
 }
 catch (Exception ex)
@@ -65,13 +58,11 @@ async Task InitializeDatabaseAsync(IHost app)
     using var scope = app.Services.CreateScope();
     var services = scope.ServiceProvider;
     var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-
     try
     {
         var userManager = services.GetRequiredService<UserManager<UserEntity>>();
         var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
         var context = services.GetRequiredService<ApplicationDbContext>();
-
         await ApplicationDbSeeder.InitializeAsync(userManager, roleManager, context, loggerFactory);
     }
     catch (Exception ex)
